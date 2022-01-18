7cd29bf9a4dae8be19860ceb7f8ba353bbeecef2
Alain Sst <AlainLuu.1999@gmail.com>
2022-01-18 20:42:34 +0700

    update code

--- main.go
+++ main.go
@@ -1,47 +1,71 @@
 package main
 
 import (
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"bytes"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
-//    go run s3_upload_object.go BUCKET_NAME FILENAME
+
+const (
+	S3_REGION = "eu-west-1"
+	S3_BUCKET = "glacial-io"
+)
+
+
 func main() {
-	if len(os.Args) != 3 {
-		exitErrorf("bucket and file name required\nUsage: %s bucket_name filename",
-			os.Args[0])
-	}
-
-	bucket := os.Args[1]
-	filename := os.Args[2]
-
-	file, err := os.Open(filename)
-	if err != nil {
-		exitErrorf("Unable to open file %q, %v", err)
-	}
-
-	defer file.Close()
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String("us-west-2")},
-	)
-
-	uploader := s3manager.NewUploader(sess)
-	_, err = uploader.Upload(&s3manager.UploadInput{
-		Bucket: aws.String(bucket),
-		Key: aws.String(filename),
-		Body: file,
+	s, err := session.NewSession(&aws.Config{
+		Region: aws.String(S3_REGION),
+		Credentials: credentials.NewStaticCredentials(
+			"XXX",
+			"YYY",
+			""),
 	})
+
 	if err != nil {
-		exitErrorf("Unable to upload %q to %q, %v", filename, bucket, err)
+		log.Fatal(err)
 	}
 
-	fmt.Printf("Successfully uploaded %q to %q\n", filename, bucket)
+	err = uploadFileToS3(s, "discharge-letter-787653.pdf")
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func exitErrorf(msg string, args ...interface{}) {
-	fmt.Fprintf(os.Stderr, msg+"\n", args...)
-	os.Exit(1)
+func uploadFileToS3(s *session.Session, fileName string) error {
+	// open the file for use
+	file, err := os.Open(fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// get the file size and read
+	// the file content into a buffer
+	fileInfo, _ := file.Stat()
+	var size = fileInfo.Size()
+	buffer := make([]byte, size)
+	file.Read(buffer)
+
+	e, s3err := s3.New(s).PutObject(&s3.PutObjectInput{
+		Bucket:               aws.String(S3_BUCKET),
+		Key:                  aws.String(fileName),
+		ACL:                  aws.String("private"),
+		Body:                 bytes.NewReader(buffer),
+		ContentLength:        aws.Int64(size),
+		ContentType:          aws.String(http.DetectContentType(buffer)),
+		ContentDisposition:   aws.String("attachment"),
+		ServerSideEncryption: aws.String("AES256"),
+		StorageClass:         aws.String("INTELLIGENT_TIERING"),
+	})
+
+	return s3err
 }
